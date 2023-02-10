88615bbab01301d1ed0a1508cc598aec720725b0
Evandro Moraes <evandroms89@gmail.com>
2023-02-10 17:38:22 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+ 
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: false
+            },
+            mensagem: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome'
+        }
+    })
+})
